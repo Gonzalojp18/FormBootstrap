62d227fcf463f31099745ac3a83d0b1c1c02faed
AlexAR93 <aleexrodriguez93@gmail.com>
2024-09-13 03:31:41 -0300

    Optimizing HTML structure and syntax

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const form = document.getElementById('form');
 const btn = [...document.querySelectorAll('.style-btn')];
 const body = document.getElementById('body');
 const allButtons = [...document.getElementsByTagName('button')];
+const allInputs = [...document.getElementsByTagName('input')];
 
 //Intercambia entre modos y también la clase 'active'
 btn.forEach(button=>{
@@ -11,21 +12,27 @@ btn.forEach(button=>{
         const secondBtn=btn.find(btnToFind=>btnToFind.textContent!==currentBtn);
 
         button.classList.add('active');
-        secondBtn.classList.remove('active')
+        secondBtn.classList.remove('active');
 
         if(currentBtn=='Basic'){
             body.classList.toggle('text-bg-dark',false);
             body.classList.toggle('text-bg-light',true);
             allButtons.forEach(btn=>{
-                btn.classList.toggle('btn-warning',false)
-                btn.classList.toggle('btn-primary',true)
+                btn.classList.toggle('btn-warning',false);
+                btn.classList.toggle('btn-primary',true);
+            })
+            allInputs.forEach(btn=>{
+                btn.classList.toggle('text-bg-dark',false);
             })
         }else{
             body.classList.toggle('text-bg-light',false);
             body.classList.toggle('text-bg-dark',true);
             allButtons.forEach(btn=>{
-                btn.classList.toggle('btn-primary',false)
-                btn.classList.toggle('btn-warning',true)
+                btn.classList.toggle('btn-primary',false);
+                btn.classList.toggle('btn-warning',true);
+            })
+            allInputs.forEach(btn=>{
+                btn.classList.toggle('text-bg-dark',true);
             })
         }
     })
@@ -38,5 +45,6 @@ form.addEventListener('submit',(e)=>{
     const inputs = form.querySelectorAll('input');
 
     alert('Your data has been successfully submitted.');
-    inputs.forEach(input => input.value = '');
+
+    inputs.forEach(input => input.value = ''); 
 })
